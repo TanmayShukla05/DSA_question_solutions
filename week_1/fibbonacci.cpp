c43b8435377088e8631938d70821924474cb4e9c
Tanmay Shukla <tanmay.shukla20051611@gmail.com>
2026-01-17 01:54:35 +0530

    fibbonacci

---
+++ week_1/fibbonacci.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+
+int fibbonacci(int n){
+    if (n<=1) return n;
+    int a = 0, b = 1;
+    int c;
+    for(int i=2;i<=n;i++){
+        c=a+b;
+        a=b;
+        b=c;
+    }
+    return b;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<fibbonacci(n);
+    return 0;
+}
